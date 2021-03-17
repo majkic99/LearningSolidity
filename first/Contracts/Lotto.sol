0463714401057f10482d7c053a3e890d9e962d1c
majkic99 <60391069+majkic99@users.noreply.github.com>
2021-03-17 18:11:03 +0100

    tested on Remix, buying tickets worked, starting the raffle from the owner works, withdrawing works. Added the option to refund if you overpaid the ticket. Will work on simplifying startRaffle by adding a variable in ticket struct

--- first/Contracts/Lotto.sol
+++ first/Contracts/Lotto.sol
@@ -1,6 +1,14 @@
 pragma solidity ^0.8.2;
-
+// SPDX-License-Identifier: MIT
 contract Lotto{
+
+    uint public aggregatePaid;
+
+    bool public done = false;
+
+    uint256 public ticketPrice = 0.1 ether;
+
+    address organiser;
 
     Ticket[] tickets;
 
@@ -12,17 +20,9 @@ contract Lotto{
 
     uint currId = 1;
 
-    uint public currBalance;
-
-    address organiser;
-
-    bool public done = false;
-
-    uint256 public ticketPrice = 0.1 ether;
-
     uint8[] resultNumbers;
 
-    function getResultNumbers() public raffleDone returns (uint8[] memory) {
+    function getResultNumbers() public raffleDone view returns (uint8[] memory) {
         return resultNumbers;
     }
 
@@ -56,13 +56,11 @@ contract Lotto{
     constructor(){
         organiser = msg.sender;
     }
-
+    //ticket must be bought before the raffle started, you enter 7 numbers between 1 and 39 as an array (format [x,x,x,x,x,x,x])
     function buyTicket(uint8[7] memory chosenNumbers) public payable raffleNotStarted returns (uint){
-        //check if balance is over 0.1 ether
-        //check if numbers are different, check if all of them are between 1 and 39
-        //create new Ticket, put in tickets and in ticketsById
-        //do event
+
         require (msg.value >= ticketPrice);
+
         for (uint i = 0; i < 7; i++){
             require (chosenNumbers[i] > 0 || chosenNumbers[i] < 40);
             for (uint j = 0; j < 7; j++){
@@ -73,17 +71,22 @@ contract Lotto{
                 }
             }
         }
+        //if you sent more money than the ticket price you can withdraw after the round is over
+        //ideas - either enable withdrawals before the raffle has ended or create another mapping and another method for returning overpaid funds
+        pendingWithdrawals[msg.sender] += (msg.value - ticketPrice);
 
         Ticket memory ticket = Ticket(currId++, chosenNumbers, msg.sender);
         tickets.push(ticket);
-        currBalance += ticketPrice;
+        aggregatePaid += ticketPrice;
+
         emit TicketBought(ticket);
+
         return ticket.id;
     }
 
     function startRaffle() onlyOrganiser raffleNotStarted public{
         //pull random numbers and put them in resultNumbers
-        //check total funds and calculate 95% to be given back to winners
+        //check totalfunds and calculate 95% to be given back to winners
         //set done to true
         //do event
         resultNumbers.push(5);
@@ -108,11 +111,11 @@ contract Lotto{
             }
         }
         //30% go to 7 hits, 20% go to 6 hits, 15% go to 5 hits, 10% go to 4 hits, 20% go to 3 hits
-        winningAmountByCorrectNumber[3] = (currBalance / 100 * 20) / numberOfWinningTicketsByCorrectNumber[3];
-        winningAmountByCorrectNumber[4] = (currBalance / 100 * 10) / numberOfWinningTicketsByCorrectNumber[4];
-        winningAmountByCorrectNumber[5] = (currBalance / 100 * 15) / numberOfWinningTicketsByCorrectNumber[5];
-        winningAmountByCorrectNumber[6] = (currBalance / 100 * 20) / numberOfWinningTicketsByCorrectNumber[6];
-        winningAmountByCorrectNumber[7] = (currBalance / 100 * 30) / numberOfWinningTicketsByCorrectNumber[7];
+        winningAmountByCorrectNumber[3] = (aggregatePaid / 100 * 20) / numberOfWinningTicketsByCorrectNumber[3];
+        winningAmountByCorrectNumber[4] = (aggregatePaid / 100 * 10) / numberOfWinningTicketsByCorrectNumber[4];
+        winningAmountByCorrectNumber[5] = (aggregatePaid / 100 * 15) / numberOfWinningTicketsByCorrectNumber[5];
+        winningAmountByCorrectNumber[6] = (aggregatePaid / 100 * 20) / numberOfWinningTicketsByCorrectNumber[6];
+        winningAmountByCorrectNumber[7] = (aggregatePaid / 100 * 30) / numberOfWinningTicketsByCorrectNumber[7];
 
         for (uint i = 0; i < tickets.length; i++){
             uint8 counter = 0;
@@ -133,11 +136,6 @@ contract Lotto{
     }
 
 
-
-    function cancelTicket(uint id) public raffleNotStarted{
-        //check if id exists, check if ticket.address matches msg.sender
-        //do event
-    }
 
     function withdrawWinnings() public payable raffleDone{
         uint amount = pendingWithdrawals[msg.sender];
